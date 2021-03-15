24dc69f7e032d4ce252f74cc62cb6d02358a90c9
Alex-prokop <prokop7@mail.ru>
2021-03-15 10:56:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,23 @@
-module.exports = function toReadable (number) {
-    
-    let first = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    let second = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    let third = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    let numberToArray = (number + '').split("");
-    let numberZeroArray = +(numberToArray[0]);
-    let numberOneArray =  +(numberToArray[1]);
-    let numberTwoArray =  +(numberToArray[2]);
-    let length = numberToArray.length;
-    if(length == 1) {
-        return first[number];
-    } else if(length == 2 && numberZeroArray == 1) {
-        return second[numberOneArray];
-    } else if(length == 2 && numberZeroArray != 1) {
-        if(numberOneArray != 0){
-            return third[numberZeroArray - 2] + ' ' + first[numberOneArray];
-        } else {
-            return third[numberZeroArray - 2];
-        }
-    } else if(length == 3 && numberOneArray == 0) {
-        if(numberTwoArray != 0){
-            return first[numberZeroArray] + ' hundred' + ' ' + first[numberTwoArray];
-        } else {
-            return first[numberZeroArray] + ' hundred';
-        }
-    } else if(length == 3 && numberOneArray == 1) {
-        return first[numberZeroArray] + ' hundred' + ' ' + second[numberTwoArray];
-    } else if(length == 3 && numberOneArray != 1) {
-        if(numberTwoArray != 0){
-            return first[numberZeroArray] + ' hundred' + ' ' + third[numberOneArray - 2] + ' ' + first[numberTwoArray];
-        } else {
-            return first[numberZeroArray] + ' hundred' + ' ' + third[numberOneArray - 2];
-        }
-    }
+const first = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const second = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+const third = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+module.exports = function toReadable(number) {
+const numArray = number.toString().split('');
+const processNumPart = (number, numberWithOffset, numberIndex, numberSize) => {
+if (numberWithOffset == null) return first[number] + (numberIndex == 0 && numberSize > 2 ? ' hundred' : '');
+switch (numberWithOffset) {
+case '0': return (numberIndex == 0 || number > 0) ? first[number] : '';
+case '1': return second[number];
+default: return third[numberWithOffset - 2] + (number > 0 ? ' ' + first[number] : '');
+}
+};
+let result = [];
+
+for (let i = numArray.length - 1; i >= 0; i -= 2) {
+var el = processNumPart(numArray[i], numArray[i - 1], i, numArray.length);
+if (el != '') result.unshift(el);
+}
+
+return result.join(' ');
 }
